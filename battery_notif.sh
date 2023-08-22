3b97d671082f86ef29dbf91fd445774722140170
MarkoMuc <marko.muc12@gmail.com>
2023-08-22 14:29:59 +0200

    Fixed logging, script needs a cleanup

--- battery_notif.sh
+++ battery_notif.sh
@@ -16,23 +16,33 @@ if [ "${BATTERY_NOTIF_OFF,,}" == "true" ]; then
     exit 0
 elif [ $val -le 25 ] && [ "$state" == "discharging" ]; then
     notify-send -u normal -t 45000 "Battery" "Plug-In charger, Battery $val%"
-    if [ $val -eq 25 ] ; then 
+    if [ $val -eq 25 ] ; then
+        datelog=$(cat $LOGS_LOCATION/battery_notif.log | cut -d "-" -f 1 | tail -n 1) 
+        datelog=$(date -d "$datelog" +%s)
+
         timelog=$(cat $LOGS_LOCATION/battery_notif.log | cut -d "-" -f 2 | tail -n 1)
         timelog=$(date -d "$timelog" +%s)
         timecurr=$(date +%s)
         
-        if [ "$((timecurr - timelog))" -gt 300 ]; then echo "$(date +"%d/%m/%Y - %T") - $val% - $state - $capacity%" >> $LOGS_LOCATION/battery_notif.log; fi
+        if [ "$datelog" -lt "$(date -d "$(date +"%m/%d/%Y")" +%s)" ] || [ "$((timecurr - timelog))" -gt 300 ]; then 
+            echo "$(date +"%m/%d/%Y - %T") - $val% - $state - $capacity%" >> $LOGS_LOCATION/battery_notif.log
+        fi
     fi
     
     exit 0
 elif [ $val -ge 75 ] && [ "$state" == "" ]; then
     notify-send -u normal -t 45000 -a Battery "Un-Plug charger, Battery $val%"
     if [ $val -eq 75 ] ; then 
+        datelog=$(cat $LOGS_LOCATION/battery_notif.log | cut -d "-" -f 1 | tail -n 1) 
+        datelog=$(date -d "$datelog" +%s)
+        
         timelog=$(cat $LOGS_LOCATION/battery_notif.log | cut -d "-" -f 2 | tail -n 1)
         timecurr=$(date +%s)
         timelog=$(date -d "$timelog" +%s)
         
-        if [ "$((timecurr - timelog))" -gt 300 ]; then echo "$(date +"%d/%m/%Y - %T") - $val% - charging - $capacity%" >> $LOGS_LOCATION/battery_notif.log; fi
+        if [ "$datelog" -lt "$(date -d "$(date +"%m/%d/%Y")" +%s)" ] || [ "$((timecurr - timelog))" -gt 300 ]; then
+            echo "$(date +"%d/%m/%Y - %T") - $val% - charging - $capacity%" >> $LOGS_LOCATION/battery_notif.log
+        fi
     fi
     exit 0
 fi
